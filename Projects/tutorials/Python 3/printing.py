1f4b9fed11024f9f7b9d246a0b81800e2ccb457f
Skiller9090 <37740659+Skiller9090@users.noreply.github.com>
2019-09-13 20:35:05 +0100

    Create printing.py

---
+++ Projects/tutorials/Python 3/printing.py
@@ -0,0 +1,6 @@
+'''
+Printing is simple.
+Printing allows you to display text onto the shell / output screen.
+Too do so type the command Print() and inside the brackets enter what you want to print...
+'''
+print("Hello, World.")
