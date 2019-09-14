380470331a4a659ab99be881e4cf09a4dc63e163
Skiller9090 <37740659+Skiller9090@users.noreply.github.com>
2019-09-14 10:05:52 +0100

    Create for loops.py

---
+++ Projects/tutorials/Python 3/for loops.py
@@ -0,0 +1,9 @@
+'''
+A for loop is a loop what iterates over a set of data...
+'''
+
+for i in range(0,15): # This loop will print all whole numbers from 0 to 15 not including 15.
+  print(i)
+
+for i in "string": # Every loop it will set i to the next letter of the string so 1st loop i == s, 2nd loop i == t and so on...
+  print(i)
