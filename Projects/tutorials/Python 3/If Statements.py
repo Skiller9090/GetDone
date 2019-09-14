5cdcf46c1d4e2eb74175a44c6140a62836209d3e
Skiller9090 <37740659+Skiller9090@users.noreply.github.com>
2019-09-14 09:52:56 +0100

    Create If Statements.py

---
+++ Projects/tutorials/Python 3/If Statements.py
@@ -0,0 +1,13 @@
+"""
+An if statement activates if the condition is true:
+"""
+
+a = 1
+b = 2
+c = 3
+
+if a == b:
+  print("This Is False") # Wont be written
+
+if b < c:
+  print("This is True) # Will be written
