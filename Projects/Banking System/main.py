6a1ddb6f4b45b6b3c51075be9c5ea43d96360c96
Skiller9090 <37740659+Skiller9090@users.noreply.github.com>
2019-09-15 13:53:25 +0100

    Update main.py

--- Projects/Banking System/main.py
+++ Projects/Banking System/main.py
@@ -1,4 +1,14 @@
 from Account import *
+import CreateAccount
 
-
-
+while command != "exit":
+  command = input("> ")
+  command_base_with_args = command.split(" ")
+  if command_base_with_args[0].lower() == "create":
+    if command_base_with_args[1].lower == "base_account":
+      CreateAccount.base_account_create()
+  elif command_base_with_args[0] == "get_info":
+    None
+    #gets all info on account
+  else:
+    print("Not A Command")
