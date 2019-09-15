b4151c7905d349a7e8347fc8fb24826036d16101
Skiller9090 <37740659+Skiller9090@users.noreply.github.com>
2019-09-15 12:49:43 +0100

    Add files via upload

--- Projects/Banking System/main.py
+++ Projects/Banking System/main.py
@@ -1,1 +1,26 @@
+import random
 
+
+def ran_16_card_num():
+    card_nums = []
+    total = 0
+    for i in range(15):
+        num = random.randint(0,9)
+        card_nums.append(str(num))
+        total += (num * (i+1))
+    check_digit = total % 10
+    card_nums.append(str(check_digit))
+    card_num = int("".join(card_nums))
+    return card_num
+
+class Account():
+    def __init__(self):
+        self.account_number = random_credit_number()
+        self.account_cards = []
+        self.account_balance = 0
+        self.loans = []
+    def load(num,cards,bal,loans):
+        self.account_number = num
+        self.account_cards = cards
+        self.account_balance = bal
+        self.loans = loans
