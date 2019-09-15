cbbc83ef56c8bc51a6f4520bf6b9aceffc6ac34e
Skiller9090 <alexspam9090@gmail.com>
2019-09-15 18:27:17 +0100

    databasing

--- Projects/Banking System/sql.py
+++ Projects/Banking System/sql.py
@@ -28,3 +28,20 @@ def save_account(conn,cursor,acc_num,acc_cards,acc_bal):
     conn.commit()
     return cursor.lastrowid
 
+def get_all_accounts(conn,cursor):
+    cursor.execute("SELECT * FROM accounts")
+    rows = cur.fetchall()
+    return rows
+def get_account(conn,cursor,account_number)
+    cursor.execute("SELECT * FROM accounts WHERE account_number = "+str(account_number))
+    rows = cur.fetchall()
+    return rows
+def update_account(conn,cursor,acc_num,acc_cards,acc_bal):
+    sql = """UPDATE accounts
+SET account_number =?
+    account_cards = ?
+    account_balance = ?
+WHERE account_number = ?"""
+    acc_cards = "|".join(acc_cards)
+    cursor.execute(sql,(acc_num,acc_cards,acc_bal,acc_num))
+    conn.commit()
