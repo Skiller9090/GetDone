cf62311de8d8315560b5d62cbdd018abb53b37bd
Skiller9090 <37740659+Skiller9090@users.noreply.github.com>
2019-09-15 12:53:50 +0100

    Update main.py

--- Projects/Banking System/main.py
+++ Projects/Banking System/main.py
@@ -15,7 +15,7 @@ def ran_16_card_num():
 
 class Account():
     def __init__(self):
-        self.account_number = random_credit_number()
+        self.account_number = ran_16_card_num()
         self.account_cards = []
         self.account_balance = 0
         self.loans = []
