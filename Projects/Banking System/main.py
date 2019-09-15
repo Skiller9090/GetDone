d950aa3c7929b7d9259f2d5d07ff97a9654f9754
Skiller9090 <37740659+Skiller9090@users.noreply.github.com>
2019-09-15 13:39:38 +0100

    Update main.py

--- Projects/Banking System/main.py
+++ Projects/Banking System/main.py
@@ -1,8 +1,5 @@
-
-import CreateAccount
-
 import random
-
+import Account
 
 def ran_16_card_num():
     card_nums = []
@@ -16,27 +13,4 @@ def ran_16_card_num():
     card_num = int("".join(card_nums))
     return card_num
 
-class Base_Account():
-    def __init__(self):
-        self.account_number = ran_16_card_num()
-        self.account_cards = []
-        self.account_balance = 0
-    def load(num,cards,bal,loans):
-        self.account_number = num
-        self.account_cards = cards
-        self.account_balance = bal
 
-def Main():
-    
-    command = ""
-    
-    
-    while command != "quit":
-        if command == "Create Account":
-            CreateAccount()
-        
-    
-    
-    
-if __name__ == "__main__":
-    Main()
