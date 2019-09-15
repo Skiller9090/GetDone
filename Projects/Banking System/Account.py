9fbc78909be8ede3bfa6747df23a7f9d7d4f3fde
Skiller9090 <alexspam9090@gmail.com>
2019-09-15 16:24:36 +0100

    new Debit Class

--- Projects/Banking System/Account.py
+++ Projects/Banking System/Account.py
@@ -6,9 +6,21 @@ class Base_Account():
         self.account_number = ran_16_card_num()
         self.account_cards = []
         self.account_balance = 0
-    def load(self,num,cards,bal,loans):
+    def load(self,num,cards,bal):
         self.account_number = num
         self.account_cards = cards
         self.account_balance = bal
     def get_all_info(self):
         return self.account_number,self.account_cards,self.account_balance
+
+class Debit_Account(Base_Account):
+    def __init__(self):
+        super().__init__()
+        self.account_loans = []
+    def load(self,num,cards,bal,loans):
+        self.account_number = num
+        self.account_cards = cards
+        self.account_balance = bal
+        self.account_loans = loans
+    def get_all_info(self):
+        return self.account_number,self.account_cards,self.account_balance,self.account_loans
