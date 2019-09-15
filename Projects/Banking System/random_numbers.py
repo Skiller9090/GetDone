1be8640fe36040d7eb395465f8a7177223b77728
Skiller9090 <37740659+Skiller9090@users.noreply.github.com>
2019-09-15 13:41:18 +0100

    Create random_numbers.py

---
+++ Projects/Banking System/random_numbers.py
@@ -0,0 +1,13 @@
+import random 
+
+def ran_16_card_num():
+    card_nums = []
+    total = 0
+    for i in range(15):
+        num = random.randint(0,9)
+        card_nums.append(str(num))
+        total += (num * (i+1))
+    check_digit = total % 10
+    card_nums.append(str(check_digit))
+    card_num = int("".join(card_nums))
+    return card_num
