3ae9f2fb9dfd3b4d34a910277b1f9747045d6581
Skiller9090 <alexspam9090@gmail.com>
2019-09-15 17:42:48 +0100

    databasing

--- Projects/Banking System/sql.py
+++ Projects/Banking System/sql.py
@@ -8,4 +8,23 @@ def create_connection(db_file):
         conn = sqlite3.connect(db_file)
     except Error as e:
         print(e)
-    return conn
+    return conn ,conn.cursor()
+
+def create_table(conn,cursor):
+    command = """CREATE TABLE IF NOT EXISTS accounts (
+    id integer PRIMARY KEY,
+    account_number integer,
+    account_cards text NOT NULL,
+    account_balance integer
+);"""
+    cursor.execute(command)
+
+def save_account(conn,cursor,acc_num,acc_cards,acc_bal):
+    sql = '''INSERT INTO accounts(account_number,account_cards,account_balance)
+    VALUES(?,?,?)
+'''
+    acc_cards = "|".join(acc_cards)
+    cursor.execute(sql,(acc_num,acc_cards,acc_bal))
+    conn.commit()
+    return cursor.lastrowid
+
