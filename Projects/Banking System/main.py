dc10f31bdcad9f55b841f8ac93d287bfdd964f4f
Skiller9090 <37740659+Skiller9090@users.noreply.github.com>
2019-09-15 12:57:00 +0100

    Add files via upload

--- Projects/Banking System/main.py
+++ Projects/Banking System/main.py
@@ -13,13 +13,20 @@ def ran_16_card_num():
     card_num = int("".join(card_nums))
     return card_num
 
-class Account():
+class Base_Account():
     def __init__(self):
         self.account_number = ran_16_card_num()
         self.account_cards = []
         self.account_balance = 0
-        self.loans = []
     def load(num,cards,bal,loans):
+        self.account_number = num
+        self.account_cards = cards
+        self.account_balance = bal
+def Simple_Account(Base_Account):
+    def __init__(self):
+        super().__init__()
+        self.loans = []
+        def load(num,cards,bal,loans):
         self.account_number = num
         self.account_cards = cards
         self.account_balance = bal
