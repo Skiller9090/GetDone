55fb16bc7fd35711fc6a68fda21f629fa4648865
rrviens <39856416+rrviens@users.noreply.github.com>
2019-09-17 21:54:11 +0300

    Revert "merge interface update"
    This reverts commit 0a2f193b95f0458754ec2e5bbee6f6b180f8e70f.

--- Projects/AA_Jack/bj.py
+++ Projects/AA_Jack/bj.py
@@ -87,7 +87,7 @@ class inputbox:
                 current_string.append(chr(inkey))
             inputbox.display_box(screen, question + ": " + "".join(current_string))
         return "".join(current_string)
-chips = {"currency":"£","amount":999999999999999999999999} 
+chips = {"currency":"£","amount":1000}
 card_value = {"A":11,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":10,"Q":10,"K":10}
 card_value_hard = {"A":1,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":10,"Q":10,"K":10}
 con = ["bust","in","lose","bj","push"]
