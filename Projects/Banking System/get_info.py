bc16282acd9720b878b324eae10f144bfcf78340
Skiller9090 <alexspam9090@gmail.com>
2019-09-17 07:16:00 +0100

    close conn

--- Projects/Banking System/get_info.py
+++ Projects/Banking System/get_info.py
@@ -3,4 +3,5 @@ from sql import *
 def get_all_info(account_num):
     conn,cursor = create_connection("./accounts.db")
     info = get_account(conn,cursor,account_num)
+    conn.close()
     return info
